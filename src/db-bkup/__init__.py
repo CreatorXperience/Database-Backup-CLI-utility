c48dc306d852ae0b802654b0f8bd6f51e9337e57
Muhammad Habeeb <allyearmustobey@gmail.com>
2024-10-02 17:34:06 +0100

    bump version 1.0.0 -> 1.1.0

--- src/db-bkup/__init__.py
+++ src/db-bkup/__init__.py
@@ -6,7 +6,7 @@ except ModuleNotFoundError as MotherFker:
     import tomli as tomllib
 
 __all__ = [""]
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 
 config_dict = tomllib.load(resources.read_text("db.bkup", "config.toml"))
